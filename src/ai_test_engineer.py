3547cbfc6f66c74d013e6006830a810c7b889adf
puttappa-raghavendra <puttappa.raghavendra@gmail.com>
2024-04-16 23:40:41 +0800

    summary file renamed to search

--- src/ai_test_engineer.py
+++ src/ai_test_engineer.py
@@ -1,5 +1,5 @@
 from api_spec_extractor import fetch_api_openapi_specification
-from api_spec_summary import search_apis
+from api_spec_search import search_apis
 from api_test_generator import api_test_case_generator
 from api_ac_generator import api_acceptence_criteria_generator
 
@@ -29,4 +29,5 @@ agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 input = '''Generate test case for the list virtual machines api'''
 
 agent_executor.invoke({"input": input})
+
 
